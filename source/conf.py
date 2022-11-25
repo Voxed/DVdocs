90fe743887d123b1c80117de9088c2d45b344236
Voxed <voxedg@gmail.com>
2022-11-25 18:57:41 +0100

    Cool stuff.

--- source/conf.py
+++ source/conf.py
@@ -1,27 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sphinx_rtd_theme
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'DVdocs'
+project = 'DViki'
 copyright = '2022, Megaman'
 author = 'Megaman'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+html_logo = "logo.png"
 
+html_theme = "sphinx_rtd_theme"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 html_static_path = ['_static']
